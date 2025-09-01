8d3418f47f1803819f7dc527538ac81bd41b63e9
chloe <g00399850@atu.ie>
2025-09-01 18:28:18 +0100

    Final update of Product Controller Class

--- src/main/java/ie/atu/productapp/ProductController.java
+++ src/main/java/ie/atu/productapp/ProductController.java
@@ -10,17 +10,43 @@ import java.util.List;
 @RequestMapping("product")
 public class ProductController {
     private List<Product> productList = new ArrayList<>();
-    public ProductController(){
-        productList.add (new Product("100", "Tv", "Electric", 399));
-        productList.add (new Product("101", "Radio", "Electric", 99));
+    public ProductController() {
+        productList.add (new Product("100","tv", "Electric", 399));
+        productList.add (new Product("101","radio", "Electric", 99));
     }
-    @GetMapping("/getProducts")
-    public List<Product> getProduct(){
+    @GetMapping("/getProduct")
+    public List<Product> getProduct()
+    {
         return productList;
     }
+
     @PostMapping("addProduct")
-    public ResponseEntity<List> addProduct(@RequestBody Product product){
+    public ResponseEntity<List> addProduct(@RequestBody Product product)
+    {
         productList.add(product);
+        return ResponseEntity.ok(productList);
+    }
+
+    @PutMapping("/updateProduct/{id}")
+    public  ResponseEntity<List> updateProduct(@PathVariable String id, @RequestBody Product product)
+    {
+        for(Product p : productList){
+            if(product.getId().equals(id)){
+                productList.remove(p);
+            }
+        }
+        productList.add(product);
+        return ResponseEntity.ok(productList);
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public ResponseEntity<List> deleteProduct(@PathVariable String id)
+    {
+        for(Product p : productList) {
+            if (p.getId().equals(id)) {
+                productList.remove(p);
+            }
+        }
         return ResponseEntity.ok(productList);
     }
 }
